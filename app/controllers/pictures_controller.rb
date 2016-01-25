794744451a1563f0ff95a447fc684d5841040d2f
naemcivic <naemcivic@gmail.com>
2016-01-25 18:19:38 -0500

    Adding feature to delete pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -35,7 +35,11 @@ class PicturesController < ApplicationController
     end
   end
 
-
+  def destroy
+   @picture = Picture.find(params[:id])
+   @picture.destroy
+   redirect_to pictures_url
+ end
 
   private
   def picture_params
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   post 'pictures' => 'pictures#create'
   get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
   patch 'pictures/:id' => "pictures#update"
+  delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
   get 'pictures/new' => 'pictures#new'
   get 'pictures/:id' => 'pictures#show', as: 'picture'
 
